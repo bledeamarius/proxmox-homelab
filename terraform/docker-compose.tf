d0ad5e5f6ce2599b48234cdcdefee4d2aa25fd49
Marius Bledea <bledea.marius97@yahoo.com>
2026-03-29 13:07:20 +0300

    Reapply "add nextcloud and tfvars example"
    This reverts commit 46ee15e68c8a9ba54f131b229322f1248c749819.

--- terraform/docker-compose.tf
+++ terraform/docker-compose.tf
@@ -1,25 +1,43 @@
-# Copy docker-compose.yml to VM
+locals {
+  docker_compose = templatefile("${path.module}/docker/docker-compose.yaml.tftpl", {
+    pihole_password = var.pihole_password
+    docker_host     = local.docker_host
+    proxmox_url     = local.proxmox_url
+    nextcloud_db_password    = var.nextcloud_db_password
+    nextcloud_admin_user     = var.nextcloud_admin_user
+    nextcloud_admin_password = var.nextcloud_admin_password
+  })
+}
+
+# Write rendered content to a local temp file
+resource "local_file" "docker_compose_rendered" {
+  content  = local.docker_compose
+  filename = "${path.module}/.rendered/docker-compose.yml"
+}
+
 resource "terraform_data" "docker_compose_sync" {
   triggers_replace = {
-    compose_hash = filemd5("${path.module}/docker/docker-compose.yaml")
+    compose_hash  = filemd5("${path.module}/docker/docker-compose.yaml.tftpl")
+    homepage_hash = filemd5("${path.module}/docker/homepage/services.yaml")
   }
 
-  depends_on = [null_resource.provision_docker_host]
+  depends_on = [terraform_data.provision_docker_host]
 
   connection {
     type        = "ssh"
     user        = "debian"
     private_key = file(local.ssh_path)
     host        = local.docker_host.ip_address
-    # bastion_host = "192.168.6.10"
-    # bastion_user = "root"
   }
 
   provisioner "file" {
-    source      = "${path.module}/docker/docker-compose.yaml"
+    source      = local_file.docker_compose_rendered.filename
     destination = "/opt/docker/docker-compose.yml"
   }
-
+#   provisioner "file" {
+#     source      = "${path.module}/docker/homepage/"
+#     destination = "/opt/homepage/config"
+#   }
   provisioner "remote-exec" {
     inline = [
       "cd /opt/docker && docker compose up -d --no-recreate"
